cdcfd07a87304f75775d5e1c5bba035fce8081fc
Vudiepk3 <110141392+Vudiepk3@users.noreply.github.com>
2025-02-18 21:42:37 +0700

    Quản lý nhiều Notification Channel trong Android

--- app/src/main/java/com/example/pushnotification/MainActivity.java
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -50,6 +50,27 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.notify(getNotificationId(), notification);
         }
     }
+    private void senNotification2() {
+        // Tạo một Bitmap từ tài nguyên drawable để làm icon lớn trong thông báo
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_round);
+
+        // Tạo một thông báo bằng Notification.Builder
+        Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID_2)
+                .setContentTitle("Title Push Notification_2") // Tiêu đề thông báo
+                .setContentText("Text Push Notification_2") // Nội dung thông báo
+                .setSmallIcon(R.mipmap.ic_launcher_round) // Icon nhỏ hiển thị trên thanh thông báo
+                .setLargeIcon(bitmap) // Icon lớn hiển thị khi mở rộng thông báo
+                .build(); // Xây dựng đối tượng thông báo
+
+        // Lấy dịch vụ NotificationManager để quản lý thông báo
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Kiểm tra nếu notificationManager không null thì gửi thông báo
+        if (notificationManager != null) {
+            notificationManager.notify(getNotificationId(), notification);
+        }
+    }
+
 
     // Phương thức tạo ID ngẫu nhiên cho mỗi thông báo bằng cách lấy thời gian hiện tại
     private int getNotificationId() {
