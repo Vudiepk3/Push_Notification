b293e946a6e6b981e5edf5ba81e25950c418e140
Vudiepk3 <110141392+Vudiepk3@users.noreply.github.com>
2025-02-19 10:59:43 +0700

    Cài đặt âm thanh cho Notification trong Android

--- app/src/main/java/com/example/pushnotification/MainActivity.java
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.pushnotification.databinding.ActivityMainBinding;
@@ -34,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private void senNotification() {
         // Tạo một Bitmap từ tài nguyên drawable để làm icon lớn trong thông báo
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_round);
-
+        // Lấy Uri của tệp âm thanh mặc định
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         // Tạo một thông báo bằng Notification.Builder
         Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID)
                 .setContentTitle("Title Push Notification") // Tiêu đề thông báo
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 .setSmallIcon(R.mipmap.ic_launcher_round) // Icon nhỏ hiển thị trên thanh thông báo
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(CONTENT))
                 .setLargeIcon(bitmap) // Icon lớn hiển thị khi mở rộng thông báo
+                .setSound(uri) // Cài đặt âm thanh cho thông báo
                 .build(); // Xây dựng đối tượng thông báo
 
         // Lấy dịch vụ NotificationManager để quản lý thông báo
@@ -55,13 +59,15 @@ public class MainActivity extends AppCompatActivity {
     private void senNotification2() {
         // Tạo một Bitmap từ tài nguyên drawable để làm icon lớn trong thông báo
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.img_notification);
-
+        // Lấy Uri của tệp âm thanh custom
+        Uri sound_notification = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sound_notification);
         // Tạo một thông báo bằng Notification.Builder
         Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID_2)
                 .setContentTitle("Title Push Notification_2") // Tiêu đề thông báo
                 .setContentText("Text Push Notification_2") // Nội dung thông báo
                 .setSmallIcon(R.mipmap.ic_launcher_round) // Icon nhỏ hiển thị trên thanh thông báo
                 .setLargeIcon(bitmap) // Icon lớn hiển thị khi mở rộng thông báo
+                .setSound(sound_notification) // Cài đặt âm thanh cho thông báo
                 .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).bigLargeIcon((Bitmap) null))
                 .build(); // Xây dựng đối tượng thông báo
 
--- app/src/main/java/com/example/pushnotification/MyApplication.java
+++ app/src/main/java/com/example/pushnotification/MyApplication.java
@@ -3,6 +3,9 @@ package com.example.pushnotification;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 
 public class MyApplication extends Application {
@@ -18,19 +21,29 @@ public class MyApplication extends Application {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is not in the Support Library.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel.
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .build();
+
             //Config channel 1
+            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
+            channel.setSound(uri, audioAttributes);
 
             //Config channel 2
+            Uri sound_notification = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sound_notification);
             CharSequence name_2 = getString(R.string.channel_name_2);
             String description_2 = getString(R.string.channel_description);
             int importance_2 = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel_2 = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
+            channel.setSound(sound_notification, audioAttributes);
+
             // Register the channel with the system. You can't change the importance
             // or other notification behaviors after this.
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
